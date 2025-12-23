67a197a70bc978c554d8eeabfce62362bbcd3801
undownding <undownding@gmail.com>
2025-12-23 13:39:39 +0800

    s3 with endpoint

--- src/s3/s3.module.ts
+++ src/s3/s3.module.ts
@@ -1,7 +1,7 @@
-import { Module } from '@nestjs/common'
-import { S3Service } from './s3.service'
-import { ConfigModule, ConfigService } from '@nestjs/config'
-import { S3_BUCKET, S3_CLIENT } from './s3.constants'
+import {Module} from '@nestjs/common'
+import {S3Service} from './s3.service'
+import {ConfigModule, ConfigService} from '@nestjs/config'
+import {S3_BUCKET, S3_CLIENT} from './s3.constants'
 
 @Module({
   imports: [ConfigModule],
@@ -15,6 +15,7 @@ import { S3_BUCKET, S3_CLIENT } from './s3.constants'
           accessKeyId: configService.get<string>('S3_ACCESS_KEY_ID'),
           secretAccessKey: configService.get<string>('S3_SECRET_ACCESS_KEY'),
           region: configService.get<string>('S3_REGION'),
+          endpoint: configService.get<string>('S3_ENDPOINT'),
           bucket: configService.get<string>('S3_BUCKET') || 'default-bucket'
         })
     },
